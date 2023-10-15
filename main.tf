cc1b86afd524e0eaddaaf8110fff888e3b2b0f9a
chinnapareddy <chinna.aduri@gmail.com>
2023-10-15 04:36:59 -0700

    state roboshop

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-/*terraform {
-  backend "s3" {}
-}
-
-variable "test" {}
-output "test" {
-value = var.test
-}*/
-
 module "components" {
-  source          = "git::https://github.com/chinna3107/p1-tf-module-basic-test.git"
-  for_each = var.components
+   source   = "git::https://github.com/chinna3107/p1-tf-module-basic-test.git"
+   for_each = var.components
 
-  zone_id         = var.zone_id
-  security_groups = var.security_groups
-  name            = each.value["name"]
-  instance_type   = each.value["instance_type"]
+   zone_id         = var.zone_id
+   security_groups = var.security_groups
+   name            = each.value["name"]
+   instance_type   = each.value["instance_type"]
+
 }
